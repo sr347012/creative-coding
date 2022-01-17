bdcce49e0407e825ebe515f2da3cf2eda25ab225
Sriramachandra <sriramachandra2387.k@gmail.com>
2022-01-17 12:58:36 +0530

    fireBrickParticles

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css'
+// import particlesJS from 'particles.js';
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js config loaded...');
+});
